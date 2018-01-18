84941cabdf0cac52f849fd82fe7a39f81b886b39
hexpang <hexpang@live.com>
2018-01-18 09:18:38 +0800

    add JsonRPC

---
+++ JsonRPC/JsonRPC/Response.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonRPC
+{
+    /// <summary>A JSON Response.</summary>
+    public abstract class Response
+    {
+        [JsonProperty("jsonrpc", Required = Required.Always)]
+        public string Version;
+
+        /// <summary>Unique Request Id.</summary>
+        [JsonProperty("id", Required = Required.Always)]
+        public int Id;
+
+        /// <summary>The error. NULL if no error occured.</summary>
+        [JsonProperty("error", Required = Required.Default)]
+        public Error Error;
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        public bool ShouldSerializeError()
+        {
+            return this.Error != null;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # WinAria
 
 ## Aria2 GUI Client for Windows
+
+## Screenshot
+
+![Screenshot](Screenshots/01.png)
+
+
+## Thanks for
+
+* (MahApps.Metro)[https://github.com/MahApps/MahApps.Metro]
+
+* (JSON.Net)[https://www.newtonsoft.com/json]
+
+* (JsonRPC)[https://github.com/adamashton/json-rpc-csharp]
