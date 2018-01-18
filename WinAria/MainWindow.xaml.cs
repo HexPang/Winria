4c893c4bcee3c61d762da9a9093ef4a701a9efb3
hexpang <hexpang@live.com>
2018-01-18 09:11:21 +0800

    first commit

---
+++ WinAria/MainWindow.xaml.cs
@@ -0,0 +1,206 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WinAria.Util;
+
+namespace WinAria
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public class MissionUri
+        {
+            public string status { get; set; }
+            public string uri { get; set; }
+        }
+        public class MissionFile
+        {
+            public string completedLength { get; set; }
+            public string index { get; set; }
+            public string length { get; set; }
+            public string path { get; set; }
+            public string select { get; set; }
+            public List<MissionUri> uris { get; set; }
+        }
+        public class MissionItem {
+            public string completedLength { get; set; }
+            public string connections { get; set; }
+            public string dir { get; set; }
+            public string downloadSpeed { get; set; }
+            public string status { get; set; }
+            public string totalLengthSize { get { return AriaUtil.GetFileSize(long.Parse(totalLength)); } }
+            public string totalLength { get; set; }
+            public string uploadLength { get; set; }
+            public string uploadSpeed { get; set; }
+            public string downloadSpeedSize { get {
+                    return AriaUtil.GetFileSize(int.Parse(downloadSpeed)) + "/s";
+                } }
+            public string gid { get; set; }
+            public string progress { get {
+                    if(long.Parse(totalLength) == 0)
+                    {
+                        return "0%";
+                    }
+                    double pst = (double.Parse(completedLength) / double.Parse(totalLength));
+                    return Math.Round((pst * 100),2) + "%";
+                }
+            }
+            public List<MissionFile> files { get; set; }
+        }
+        public ObservableCollection<MissionItem> MissionList;
+        public ObservableCollection<MissionItem> WaittingMissionList;
+        public ObservableCollection<MissionItem> PausedMissionList;
+
+        private Timer timer = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!AriaUtil.IsRunning())
+            {
+                AriaUtil.Start();
+            }
+            else
+            {
+                reloadList();
+            }
+            timer = new Timer();
+            timer.Enabled = true;
+            timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            reloadList();
+        }
+        private void reloadListSource()
+        {
+            if (missionListView == null) return;
+            this.Invoke(() => {
+                if (StateList.SelectedIndex == 0)
+                {
+                    missionListView.ItemsSource = MissionList;
+                }else if(StateList.SelectedIndex == 1)
+                {
+                    missionListView.ItemsSource = PausedMissionList;
+                }else
+                {
+                    missionListView.ItemsSource = WaittingMissionList;
+                }
+                ListBoxItem item = StateList.Items[0] as ListBoxItem;
+                if(MissionList != null)
+                    item.Content = "Active(" + MissionList.Count + ")";
+                item = StateList.Items[1] as ListBoxItem;
+                if(PausedMissionList != null) item.Content = "Paused(" + PausedMissionList.Count + ")";
+                item = StateList.Items[2] as ListBoxItem;
+                if(WaittingMissionList != null) item.Content = "Waiting(" + WaittingMissionList.Count + ")";
+            });
+
+        }
+        private void reloadList()
+        {
+            AriaUtil.JsonRequestAsync("aria2.tellActive", null, (ent) => {
+                if (ent.Result != null)
+                {
+                    MissionList = ent.Result.ToObject<ObservableCollection<MissionItem>>();
+                    reloadListSource();
+                }
+            });
+            AriaUtil.JsonRequestAsync("aria2.tellStopped", JToken.FromObject(new List<int> { 0, 9999 }), (ent) => {
+                if (ent.Result != null)
+                {
+                    PausedMissionList = ent.Result.ToObject<ObservableCollection<MissionItem>>();
+                }
+            });
+            AriaUtil.JsonRequestAsync("aria2.tellWaiting", JToken.FromObject(new List<int> { 0, 9999 }), (ent) => {
+                if (ent.Result != null)
+                {
+                    WaittingMissionList = ent.Result.ToObject<ObservableCollection<MissionItem>>();
+                }
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ShowMessageAsync();
+        }
+
+        private async Task ShowMessageAsync()
+        {
+
+            var input = await this.ShowInputAsync("WinAria", "Please enter URL or Magnet address");
+            if(input != null)
+            {
+                List<List<string>> files = new List<List<string>> { new List<string> { input } };
+                AriaUtil.JsonRequestAsync("aria2.addUri", JToken.FromObject(files),(ent)=> {
+                    if(ent.Result != null)
+                    {
+                        reloadList();
+                    }
+                });
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            string gid = btn.Tag as string;
+            AriaUtil.JsonRequestAsync("aria2.pause", JToken.FromObject(new List<string> { gid }), (ent) => {
+                reloadList();
+            });
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            string gid = btn.Tag as string;
+            AriaUtil.JsonRequestAsync("aria2.remove", JToken.FromObject(new List<string> { gid }), (ent) => {
+                reloadList();
+            });
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            string gid = btn.Tag as string;
+            AriaUtil.JsonRequestAsync("aria2.unpause", JToken.FromObject(new List<string> { gid }), (ent) => {
+                reloadList();
+            });
+        }
+
+        private void StateList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            reloadListSource();
+        }
+
+        private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            AriaUtil.JsonRequestAsync("aria2.shutdown", null, (ent) => {
+
+            });
+        }
+    }
+}
---
+++ WinAria/Util/AriaUtil.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonRPC;
+using Newtonsoft.Json.Linq;
+
+namespace WinAria.Util
+{
+    public delegate void JsonRPCResponseEventHandler(JsonRPCResponse result);
+    public class JsonRPCResponse
+    {
+        public JToken Result { get; set; }
+    }
+    public class AriaUtil
+    {
+		public static bool IsRunning()
+        {
+            Process[] processes = Process.GetProcessesByName("aria2c");
+            return processes.Length == 0 ? false : true;
+        }
+
+		public static void Start()
+        { 
+            string binaryPath = AppDomain.CurrentDomain.BaseDirectory + @"binary\";
+            ProcessStartInfo startInfo = new ProcessStartInfo(binaryPath + "aria2c.exe", "--enable-rpc=true --input-file=" + binaryPath + "aria2.session --conf-path=" + binaryPath + "aria2.conf");
+            startInfo.WorkingDirectory = binaryPath;
+            //startInfo.UseShellExecute = true;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(startInfo);
+        }
+
+        public static void JsonRequestAsync(string method,JToken param,JsonRPCResponseEventHandler eventHandler)
+        {
+            try
+            {
+                using (Client rpcClient = new Client("http://127.0.0.1:10086/jsonrpc"))
+                {
+                    Request req = rpcClient.NewRequest(method, param);
+                    GenericResponse response = rpcClient.Rpc(req);
+                    JToken result = null;
+                    if (response.Result != null)
+                    {
+                        result = response.Result;
+                    }
+                    eventHandler(new JsonRPCResponse() { Result = result });
+                }
+            }
+            catch (Exception)
+            {
+                eventHandler(new JsonRPCResponse() { Result = null });
+            }
+
+        }
+
+
+        public static string GetFileSize(long size)
+        {
+            var num = 1024.00; //byte
+
+            if (size < num)
+                return size + "B";
+            if (size < Math.Pow(num, 2))
+                return (size / num).ToString("f2") + "K"; //kb
+            if (size < Math.Pow(num, 3))
+                return (size / Math.Pow(num, 2)).ToString("f2") + "M"; //M
+            if (size < Math.Pow(num, 4))
+                return (size / Math.Pow(num, 3)).ToString("f2") + "G"; //G
+
+            return (size / Math.Pow(num, 4)).ToString("f2") + "T"; //T
+        }
+    }
+}
